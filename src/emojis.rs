e1f655a59c5d4ce39fd8c17af59f197a3d16dfb3
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-10-12 17:05:34 -0400

    add emoji replacement code

--- src/emojis.rs
+++ src/emojis.rs
@@ -1,3 +1,37 @@
 pub(crate) fn replace_emojis(input: &str) -> String {
-    todo!()
+    let mut builder = String::new();
+    let mut stack = String::new();
+
+    for char in input.chars() {
+        match char {
+            ' ' => {
+                builder += process_stack(&stack);
+                stack.clear();
+                builder.push(char);
+            }
+            _ => stack.push(char),
+        }
+    }
+
+    builder += process_stack(&stack);
+    builder
+}
+
+fn process_stack<'a>(stack: &'a str) -> &'a str {
+    match stack {
+        ":)" => "🙂",
+        ":(" => " 🙁",
+        ">:)" => "😈",
+        ">:(" => "😠",
+        ":/" => "🫤",
+        ";)" => "😉",
+        ":D" => "😁",
+        "xD" => "😆",
+        ":p" => "😛",
+        ";p" => "😜",
+        "xp" => "😝",
+        ":|" => "😐",
+        ":O" => "😮",
+        _ => stack,
+    }
 }
